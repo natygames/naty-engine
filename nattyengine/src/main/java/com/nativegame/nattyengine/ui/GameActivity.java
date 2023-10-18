6662cf008acb927f8a0c6398111623b4bb2309dd
oscarliang7 <a7757729@gmail.com>
2023-10-18 17:07:39 +0800

    Fix showing full screen when window focus change

--- nattyengine/src/main/java/com/nativegame/nattyengine/ui/GameActivity.java
+++ nattyengine/src/main/java/com/nativegame/nattyengine/ui/GameActivity.java
@@ -62,6 +62,18 @@ public class GameActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setVolumeControlStream(android.media.AudioManager.STREAM_MUSIC);
